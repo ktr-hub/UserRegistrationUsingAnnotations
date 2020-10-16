acce6150359d8bb038d9975d02c4261fd7300420
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-16 06:24:51 +0530

    [Tirupathi] Add. Welcome message into master

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationUsingAnnotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration using annotations!");
+        }
+    }
+}
